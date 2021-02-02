46798b3cd0727a89e2137083329f79dc01f285e3
Lisa Doan <ldoan24@gmail.com>
2021-02-02 12:55:38 -0800

    Resubmission javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,5 +1,5 @@
 var click = 0;
-
+var $back = document.querySelector('.backlight');
 var $clickButton = document.querySelector('.light');
 $clickButton.addEventListener('click', function (event) {
   click++;
@@ -10,15 +10,5 @@ $clickButton.addEventListener('click', function (event) {
     onOrOff = 'off';
   }
   $clickButton.className = 'light ' + onOrOff;
-});
-
-var $back = document.querySelector('.backlight');
-$back.addEventListener('click', function (event) {
-  var onOrOff = '';
-  if (click % 2 === 0) {
-    onOrOff = 'on';
-  } else {
-    onOrOff = 'off';
-  }
   $back.className = 'backlight ' + onOrOff;
 });
