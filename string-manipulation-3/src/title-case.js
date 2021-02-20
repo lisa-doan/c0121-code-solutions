b51b4a682114fb285b81de23ff27678e686df81d
Lisa Doan <ldoan24@gmail.com>
2021-02-20 14:22:02 -0800

    Completed string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,12 +1,38 @@
 /* exported titleCase */
+
+var specialCases = {
+  javascript: 'JavaScript',
+  'javascript:': 'JavaScript:',
+  api: 'API',
+  web: 'Web'
+};
+var minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+
 function titleCase(string) {
-  var splitStringArray = string.split('');
-  var newString = '';
-  for (var i = 0; i < splitStringArray.length; i++) {
-    if (splitStringArray.length >= 4) {
-      newString += splitStringArray[i];
+  var stringArray = string.toLowerCase().split(' ');
+  for (var i = 0; i < stringArray.length; i++) {
+    if (stringArray[i].includes(':')) {
+      stringArray[i + 1] = stringArray[i + 1].charAt(0).toUpperCase() + stringArray[i + 1].slice(1);
+    }
+    if (stringArray[i].includes('-')) {
+      stringArray[i] = capitalizeHyphen(stringArray[i]);
+    }
+    if (stringArray[i] in specialCases) {
+      stringArray[i] = specialCases[stringArray[i]];
+    } else if (!minorWords.includes(stringArray[i])) {
+      stringArray[i] = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1);
     }
   }
+  var newString = stringArray.join(' ');
   return newString;
+}
 
+function capitalizeHyphen(string) {
+  var splitHyphenWord = string.trim().split('-');
+  var output = [];
+  for (var j = 0; j < splitHyphenWord.length; j++) {
+    output.push(splitHyphenWord[j].charAt(0).toUpperCase() + splitHyphenWord[j].slice(1));
+  }
+  var newWord = output.join('-');
+  return newWord;
 }
