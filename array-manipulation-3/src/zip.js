3022110d393ba0b83d505e4ee0eea6f62179165e
lisadoann <ldoan24@gmail.com>
2021-02-23 05:12:49 +0000

    Made changes to zip function

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,21 +1,12 @@
 /* exported zip */
 function zip(first, second){
     var output = [];
-    if(first.length > second.length) {
-        for(var i = 0; i < second.length ; i++) {
-            var subArray = [];
-            subArray.push(first[i])
-            subArray.push(second[i]);
-            output.push(subArray);
-        }
-    }
-    else if(second.length >= first.length) {
-        for(var i = 0; i < first.length ; i++) {
-            var subArray = [];
-            subArray.push(first[i])
-            subArray.push(second[i]);
-            output.push(subArray);
-        }
+    var length = Math.min(first.length, second.length);
+    for(var i = 0; i < length; i++) {
+        var subArray = [];
+        subArray.push(first[i])
+        subArray.push(second[i]);
+        output.push(subArray);
     }
     return output
 }
